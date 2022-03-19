4a196410d5bec044369d51a202109b8f11a56fe6
hafizfqb <63943967+fullstack-nico@users.noreply.github.com>
2022-03-19 22:29:08 +0800

    Login and dashboard navigation logic

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import Index from './src/app/Index';
+import SrcMain from './src/SrcMain';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Index);
+AppRegistry.registerComponent(appName, () => SrcMain);
